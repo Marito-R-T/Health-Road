ee8bf7b84518a6b6c704c79635387f7dadcdfacc
jhonny <ismaelherz2001@gmail.com>
2021-09-23 17:50:40 -0600

    implementar view engine

--- web/app.js
+++ web/app.js
@@ -9,12 +9,18 @@ var body_parser = require('body-parser');
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({extended:true}));
 
+app.set('view engine', 'ejs');
+
 //routes
 var {hospital_router}=require('./routes/hospital');
 var {services_router}=require('./routes/services');
 
 app.use('/hospital',hospital_router);
 app.use('/service',services_router);
+app.get('/main', (req, res) => {
+  res.render('prueba')
+})
+
 
 
 app.listen(port, () => console.log(`Example app listening on port port!`));
