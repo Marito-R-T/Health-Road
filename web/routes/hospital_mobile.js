e7f9f54c2937377443c32332be2c0448da90f91c
jhonny <ismaelherz2001@gmail.com>
2021-10-08 12:46:48 -0600

    Filtrar hospital por nombre

--- web/routes/hospital_mobile.js
+++ web/routes/hospital_mobile.js
@@ -24,4 +24,26 @@ router.get("/info/",(req, res)=>{
     })
 })
 
+//search hospital history 56
+router.get("/info/",(req, res)=>{
+    user.findAll({
+        include:{
+            model:hospital,
+        },
+        where:{
+            user: sequelize.where(sequelize.fn('LOWER', sequelize.col('user')), 'LIKE', '%' + req.body.user.toLowerCase() + '%'),
+        }
+    })
+    .then(e=>{
+        if(e){
+            res.status(201).json(e)
+        }else{
+            res.status(401).json({ error:"No se encontro el hospital"})
+        }
+    })
+    .catch(err=>{
+        res.status(501).json({ error: "No se encontro el hospital,intente de nuevo"})
+    })
+})
+
 module.exports.hospital_router_mobile = router;
