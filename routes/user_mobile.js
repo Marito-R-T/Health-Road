cbc5e5db59c4a6ae189f0029c952b365309e5669
jhonny <ismaelherz2001@gmail.com>
2021-10-22 02:00:47 -0600

    Update usuario

--- routes/user_mobile.js
+++ routes/user_mobile.js
@@ -80,11 +80,12 @@ router.put('/update/',(req, res) => {
         }
     ).then(e=>{
         if(e && e[0]){
-            res.status(201).json(
-                user.findOne({
-                    where: {user:user_, rol:3}
-                })
-            )
+            user.findOne({
+                where: {user:user_, rol:3}
+            }).then(e=>{
+                res.status(201).json(e)
+            })
+            
         }else{
             res.status(400).json({ error:"No se pudo actualizar el perfil, intente de nuevo"})
         }
